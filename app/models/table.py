b5b8ce8afc1bdef3d2df26281aadb0f5a9fc3638
HolyFrancis <holyfrancisdushime@gmail.com>
2024-02-22 23:10:58 +0200

    new attributes

--- app/models/table.py
+++ app/models/table.py
@@ -4,7 +4,8 @@ from django.utils.translation import gettext as _
 
 class Place(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
-    
+    short_description = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.name
     
@@ -22,6 +23,7 @@ class Table(models.Model):
         verbose_name=_("Place")
     )
     image = models.ImageField(help_text=_("Table Image"), upload_to=None, height_field=None, width_field=None, max_length=None)
+    created_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return self.name
