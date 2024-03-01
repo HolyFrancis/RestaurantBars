578fbaaa5695e2e30d5c5f7e7113c76297326c8e
HolyFrancis <holyfrancisdushime@gmail.com>
2024-03-01 16:48:11 +0200

    some packages in installed app

--- RestoBar/settings.py
+++ RestoBar/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
-    'rest_framework'
+    'rest_framework',
+    'widget_tweaks',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
